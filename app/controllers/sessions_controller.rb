ba4f17154067c0d9e2365c71950390efb2b6b395
Susha Dore <Guest@Epicodus-2G.local>
2017-07-05 16:49:46 -0700

    initialize files and start building page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  # def create
+  #   @user = User.authenticate(params[:email], params[:password])
+  #   if @user
+  #     session[:user_id] = @user.id
+  #     redirect_to "/"
+  #   else
+  #     redirect_to signin_path
+  #   end
+  # end
+  #
+  # def destroy
+  #   session[:user_id] = nil
+  #   flash[:notice] = "You've signed out."
+  #   redirect_to "/"
+  # end
+end
